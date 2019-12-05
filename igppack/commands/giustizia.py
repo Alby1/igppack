6f1338a8af92dc2d86349f4b850fbc6569dfe8f6
Alby1 <30355916+Alby1@users.noreply.github.com>
2019-12-05 09:18:00 +0100

    fixed permessi

--- igppack/commands/giustizia.py
+++ igppack/commands/giustizia.py
@@ -20,39 +20,38 @@ class GiustiziaCommand(Command):
         username = args.optional(0)
         variazione = args.optional(1)
         blocco = await data.get_author(error_if_none=True)
-        if blocco is not 1:
-            await data.reply(f"Non sei autorizzato a usare questo comando.")
-
-        if username is None:
-            user: User = await data.get_author(error_if_none=True)
+        if blocco is not 1 and variazione is not None:
+            await data.reply(f"Non sei autorizzato a modificare le giustizie.")
         else:
-            found: Optional[User] = await asyncify(
-                data.session
-                    .query(self.alchemy.get(User))
-                    .filter(func.lower(self.alchemy.get(User).username) == func.lower(username))
-                    .one_or_none
-            )
-            if not found:
-                raise InvalidInputError("Utente non trovato.")
+            if username is None:
+                user: User = await data.get_author(error_if_none=True)
             else:
-                user = found
+                found: Optional[User] = await asyncify(
+                    data.session
+                        .query(self.alchemy.get(User))
+                        .filter(func.lower(self.alchemy.get(User).username) == func.lower(username))
+                        .one_or_none
+                )
+                if not found:
+                    raise InvalidInputError("Utente non trovato.")
+                else:
+                    user = found
 
+            if user.giustizia is None:
 
-        if user.giustizia is None:
+                giustizia = self.alchemy.get(Giustizia)(user = user, conteggio = 0)
+                data.session.add(giustizia)
+                await data.session_commit()
 
-            giustizia = self.alchemy.get(Giustizia)(user = user, conteggio = 0)
-            data.session.add(giustizia)
-            await data.session_commit()
+            else:
+                giustizia = user.giustizia
 
-        else:
-            giustizia = user.giustizia
+            if variazione is not None:
+                varint = int(variazione)
+                giustizia.conteggio = giustizia.conteggio + varint
+                await data.session_commit()
 
-        if variazione is not None:
-            varint = int(variazione)
-            giustizia.conteggio = giustizia.conteggio + varint
-            await data.session_commit()
-
-        await data.reply(f"{user.username} sarà giustiziato: {giustizia.conteggio} volte")
+            await data.reply(f"{user.username} sarà giustiziato: {giustizia.conteggio} volte")
 
 
 
